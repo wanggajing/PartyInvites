485c01b3cb8215c3e65449d927a49db81fb6e729
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-12 17:39:12 +1300

    Essential Tools

---
+++ EssentialTools/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="EssentialTools_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="../EssentialTools.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div><label>Name:</label><input id="name" runat="server"/></div>
+        <div><label>City:</label><input id="city" runat="server" /></div>
+        <button type="submit">Submit</button>
+    </form>
+    <p id="target" runat="server"></p>
+</body>
+</html>
---
+++ EssentialTools/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class EssentialTools_Default : System.Web.UI.Page
+{
+    public class FormData
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            FormData dataObject = new FormData();
+            if (TryUpdateModel(dataObject,
+            new FormValueProvider(ModelBindingExecutionContext)))
+            {
+                target.InnerText = String.Format("Name: {0}, City: {1}",
+                dataObject.Name, dataObject.City);
+            }
+        }
+    }
+}
