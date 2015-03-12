640fd9597cd42e34f5a2697424651d9f47146e30
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-12 14:31:56 +1300

    Asynchourous methond

---
+++ App_Code/MyAsyncMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for MyAsyncMethods
+/// </summary>
+public class MyAsyncMethods
+{
+    public static Task<long?> GetPageLength()
+    {
+        HttpClient client = new HttpClient();
+        var httpTask = client.GetAsync("http://apress.com");
+        // we could do other things here while we are waiting
+        // for the HTTP request to complete
+        return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+        {
+            return antecedent.Result.Content.Headers.ContentLength;
+        });
+    }
+}
