b795750431c99a686963c5157815f8c468533544
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-12 17:10:54 +1300

    dynamically fill table data with JSON

---
+++ JSON/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="JSON_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Summits</title>
+    <link href="../Styles.css" rel="stylesheet" />
+    <script src="../Scripts/jquery-2.1.3.js"></script>
+    <script src="../Scripts/JSON.js"></script>
+</head>
+<body>
+    <h2>Summits</h2>
+    <table id="peaksTable">
+        <thead><tr><th class="name">Name</th><th>Height (m)</th></tr></thead>
+        <tbody id="tableBody"></tbody>
+    </table>
+    <input type="button" value="Delete" />
+</body>
+</html>
---
+++ Scripts/JSON.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="jquery-2.1.3.js" />
+$(document).ready(function () {
+    var jsonString = '[{"Name":"Everest","Height":8848},'
+    + '{"Name":"Aconcagua","Height":6962},{"Name":"McKinley","Height":6194},'
+    + '{"Name":"Kilimanjaro","Height":5895},{"Name":"K2","Height":8611}]';
+    var dataObjects = $.parseJSON(jsonString);
+    var targetElem = $('#tableBody');
+    for (var i = 0; i < dataObjects.length; i++) {
+        targetElem.append('<tr><td class="name">'
+        + dataObjects[i].Name + '</td><td class="height">'
+        + dataObjects[i].Height + '</td></tr>');
+    }
+    $('tr').prepend("<td></td>");
+    $('<input name="delete" type="radio"/>').prependTo('tbody td:first-child')
+    .first().attr("checked", true);
+    $(':button').click(function (e) {
+        $(':radio:checked').closest('tr').remove();
+        $(':radio').first().attr("checked", true);
+    });
+});
